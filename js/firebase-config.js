30af37b65b584bf789c3ff7baa7fe9d520edee45
anthonylindovsk <anthony.enzo1234@gmail.com>
2026-08-24 16:43:09 -0300

    Include Firebase Auth in firebase-config.js
    Added Firebase Authentication to the configuration.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+ 
 const firebaseConfig = {
   apiKey: "AIzaSyD3d5yKWu5hjggINLWUGmJEG6XbngVP9IE",
   authDomain: "portal-mesada.firebaseapp.com",
@@ -9,7 +10,8 @@ const firebaseConfig = {
   messagingSenderId: "237825551972",
   appId: "1:237825551972:web:53066aed9e287b354ab971"
 };
-
+ 
 const app = initializeApp(firebaseConfig);
-
+ 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
