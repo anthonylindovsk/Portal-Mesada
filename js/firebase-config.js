ee08f25fe555c5e4585f4e4f57e340b0fb45bcb7
anthonylindovsk <anthony.enzo1234@gmail.com>
2026-08-27 17:09:15 -0300

    Refactor firebase-config to include additional Firestore exports

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
- 
+import { getFirestore, collection, addDoc, Timestamp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyD3d5yKWu5hjggINLWUGmJEG6XbngVP9IE",
   authDomain: "portal-mesada.firebaseapp.com",
   projectId: "portal-mesada",
   storageBucket: "portal-mesada.firebasestorage.app",
   messagingSenderId: "237825551972",
-  appId: "1:237825551972:web:53066aed9e287b354ab971"
+  appId: "1:237825551972:web:53066aed9e287b354ab971",
 };
- 
-const app = initializeApp(firebaseConfig);
- 
+
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+export { collection, addDoc, Timestamp };
