a78cced8f17cccd6ab25c0b7ec042d647021d2d3
anthonylindovsk <anthony.enzo1234@gmail.com>
2026-08-27 17:07:26 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function mostrarAba(nomeDaAba) {
-  document.getElementById("pfazer").style.display = "none";
-  document.getElementById("pconcluidas").style.display = "none";
-  document.getElementById(nomeDaAba).style.display = "block";
-}
-
-mostrarAba("pfazer");
