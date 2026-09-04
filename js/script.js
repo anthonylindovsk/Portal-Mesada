eece7269da5395c92860c6116af7bd413221fff0
anthonylindovsk <anthony.enzo1234@gmail.com>
2026-09-04 15:07:51 -0300

    Add handling for 'pperdidas' tab in mostrarAba function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function mostrarAba(nomeDaAba) {
- document.getElementById("pfazer").style.display = "none";
+  document.getElementById("pfazer").style.display = "none";
   document.getElementById("pconcluidas").style.display = "none";
+  document.getElementById("pperdidas").style.display = "none";
   document.getElementById(nomeDaAba).style.display = "block";
 }
 
